e862d34d33884d1c3221fa959726fbe1abe86511
Simran <sw219335@gmail.com>
2024-07-25 23:35:12 +0530

    next commit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slidesIframe = document.createElement('iframe');
-    slidesIframe.src = 'https://docs.google.com/presentation/d/e/2PACX-1vT5qxX8FYzM3GaSyfO5Xc2tj3sLZ-9tI0hH5Wz0lFYFP3fuDrpl4fh4S84vUae6/pub?start=false&loop=false&delayms=3000';
-    slidesIframe.width = '960';
-    slidesIframe.height = '569';
-    slidesIframe.allowFullscreen = true;
+// document.addEventListener('DOMContentLoaded', function () {
+//     const slidesIframe = document.createElement('iframe');
+//     slidesIframe.src = 'https://docs.google.com/presentation/d/e/2PACX-1vT5qxX8FYzM3GaSyfO5Xc2tj3sLZ-9tI0hH5Wz0lFYFP3fuDrpl4fh4S84vUae6/pub?start=false&loop=false&delayms=3000';
+//     slidesIframe.width = '960';
+//     slidesIframe.height = '569';
+//     slidesIframe.allowFullscreen = true;
     
-    const widgetContainer = document.getElementById('google-slides-widget');
-    widgetContainer.appendChild(slidesIframe);
-  });
+//     const widgetContainer = document.getElementById('google-slides-widget');
+//     widgetContainer.appendChild(slidesIframe);
+//   });
   
+// document.addEventListener('DOMContentLoaded', function() {
+//   const linkContainer = document.getElementById('link-container');
+//   const link = document.createElement('a');
+//   link.href = 'https://docs.google.com/presentation/d/1KsbbL_kH5gXXHDBfChBUVVY0bHBYf_d4bW5M6t2xEOw/edit#slide=id.g2ee6b31cec3_0_6';
+//   link.target = '_blank';
+//   link.textContent = 'View the presentation';
+//   linkContainer.appendChild(link);
+// });
