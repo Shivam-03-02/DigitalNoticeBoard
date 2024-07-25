f76b6e892cbe795b344377a8b82796f577e5e54d
Simran <sw219335@gmail.com>
2024-07-26 01:00:47 +0530

    ninth

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slidesIframe = document.createElement('iframe');
-    slidesIframe.src = 'https://docs.google.com/presentation/d/e/2PACX-1vT5qxX8FYzM3GaSyfO5Xc2tj3sLZ-9tI0hH5Wz0lFYFP3fuDrpl4fh4S84vUae6/pub?start=false&loop=false&delayms=3000';
-    slidesIframe.width = '960';
-    slidesIframe.height = '569';
-    slidesIframe.allowFullscreen = true;
+// document.addEventListener('DOMContentLoaded', function () {
+//     const slidesIframe = document.createElement('iframe');
+//     slidesIframe.src = 'https://docs.google.com/presentation/d/e/2PACX-1vT5qxX8FYzM3GaSyfO5Xc2tj3sLZ-9tI0hH5Wz0lFYFP3fuDrpl4fh4S84vUae6/pub?start=false&loop=false&delayms=3000';
+//     slidesIframe.width = '960';
+//     slidesIframe.height = '569';
+//     slidesIframe.allowFullscreen = true;
     
-    const widgetContainer = document.getElementById('google-slides-widget');
-    widgetContainer.appendChild(slidesIframe);
-  });
+//     const widgetContainer = document.getElementById('google-slides-widget');
+//     widgetContainer.appendChild(slidesIframe);
+//   });
   
